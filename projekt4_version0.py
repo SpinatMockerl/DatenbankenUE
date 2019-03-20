78806adaf5402f412790aa9f4e0828f51fa50679
Eva Lehner <eva.lehner@stud.fh-campuswien.ac.at>
2019-03-20 15:01:50 +0100

    Registration is possible when primary key constrain is met. Redirection to home and login. No nice notifiaction what happened implemented.

--- projekt4_version0.py
+++ projekt4_version0.py
@@ -2,11 +2,9 @@
 # coding: utf8
 
 import sqlite3
-from flask import Flask, render_template, make_response, request, session
+from flask import Flask, render_template, make_response, request, session, redirect
 import os
 
-
-#####################################################################################################################
 # Tutorial: https://www.youtube.com/watch?v=o-vsdfCBpsU
 
 # define connection, sqlite will create db file if not already there
@@ -17,17 +15,17 @@ curs = connection.cursor()
 
 # Create Tables
 # enter sql statements in doc string format
-curs.execute('''CREATE TABLE IF NOT EXISTS
-    Person(SVNr int(10), Vorname varchar(10),
-    Nachname varchar(10), PLZ int, Ort varchar(10),
-    Straße varchar(50), HausNr int(3), TelefonNr int, PRIMARY KEY (SVNr))''')
+curs.execute('''CREATE TABLE IF NOT EXISTS 
+    Person(SVNr int(10), Vorname varchar(10), 
+    Nachname varchar(10), PLZ int, Ort varchar(10), 
+    Strasse varchar(50), HausNr int(3), TelefonNr int, PRIMARY KEY (SVNr))''')
 
-curs.execute('''CREATE TABLE IF NOT EXISTS Telefonnummer( SVNr int(10), Telefon_nr int(12),
+curs.execute('''CREATE TABLE IF NOT EXISTS Telefonnummer( SVNr int(10), Telefon_nr int(12), \
     PRIMARY KEY (SVNr, Telefon_nr), FOREIGN KEY (SVNr) REFERENCES Personen(SVNr))''')
 
 # insert data (if it does not exist)
 try:
-    curs.execute('''INSERT INTO Person VALUES('1234567890', 'Laura', 'Lama', '2020', 'Wien', 'Zoostraße', '20', '5555')''')
+    curs.execute('''INSERT INTO Person VALUES('1234567890', 'Laura', 'Lama', '2020', 'Wien', 'Zoostrasse', '20', '5555')''')
 except sqlite3.IntegrityError as error:
     print(error)
     print('Data does already exist')
@@ -37,14 +35,11 @@ connection.commit()
 # close stuff
 curs.close()
 connection.close()
-#####################################################################################################################
 
 # Flask constructior: define application as Flask object
 app = Flask(__name__)
-app.secret_key = os.urandom(24)
+app.secret_key = os.urandom(24) # secret_key later used in session setup
 
-# Flask route() function: tells app which URL should be called
-# used to bind URL to function ( also possible with .add_url_rule() )
 @app.route('/home')
 def home():
     return render_template(('home.html'))
@@ -67,7 +62,6 @@ def login():
             if SvNr == SvNr_SQL:
                 session['SvNr'] = SvNr
                 return 'User exists'
-                #return render_template(('person.html'))
         except TypeError as error:
             print(error)
             return render_template('register.html')
@@ -76,7 +70,7 @@ def login():
 
 
 
-@app.route('/register', methods=["POST", "GET"])
+@app.route('/register', methods=["POST"])
 # mehrere Telefon nummer nicht implementiert
 def register():
     try:
@@ -91,30 +85,30 @@ def addPerson():
     if request.method == "POST":
         SvNr = request.form['SvNr']
         Vorname = request.form["Vorname"]
+        Nachname = request.form["Nachname"]
         PLZ = request.form["PLZ"]
         Ort = request.form["Ort"]
-        Straße = request.form["Straße"]
+        Strasse = request.form["Strasse"]
+        print(Strasse)
         HausNr = request.form["HausNr"]
         TelefonNr = request.form["TelefonNr"]
 
         try:
             connection = sqlite3.connect('DatabaseVersion0.db')
             curs = connection.cursor()
-            curs.execute("INSERT INTO Person ('SvNr', 'Vorname', 'Nachname', 'PLZ', 'Ort', 'Straße', 'HausNr', 'TelefonNr') VALUES (?,?,?,?,?,?,?,?)")
-            curs.commit()
+            curs.execute("INSERT INTO PERSON (SvNr, Vorname, Nachname, PLZ, Ort, Strasse, HausNr, TelefonNr) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", (SvNr, Vorname, Nachname, PLZ, Ort, Strasse, HausNr, TelefonNr))
+            connection.commit()
             curs.close()
 
-            return render_template(('addPerson.html'))
+            return render_template(('home.html'))
 
         except sqlite3.IntegrityError as error:
             connection.rollback()
             return render_template(('home.html'))
         finally:
             connection.close()
-            #return 'Reached finally'
-            return render_template(('addPerson.html', ))
-    else:
-        return '<h1>get method was used tis is not save! </h1>'
+    #else:
+        # return '<h1>get method was used tis is not save! </h1>'
 
 
 
@@ -122,15 +116,6 @@ def addPerson():
 def userEnvironment():
     identification = request.cookies.get('SvNr')
     return '<h1>welcome ' + identification + '</h1>'
-
-
-#@app.route('/insertPerson')
-# access page: http://localhost:5000/insertPerson
-#def newPerson():
-#    return render_template('person.html')
-
-
-
 
 
 if __name__ == '__main__':
